e63a4eff8e34b2c886fe6cc43c741d95e9e974c5
Karthik <tanuku.saikarthik@gmail.com>
2024-03-24 01:16:53 +0530

    express

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import submissionsRouter from './submissions.js';
-
-const router = express.Router();
-
-router.use('/submissions', submissionsRouter);
-
-export default router;
